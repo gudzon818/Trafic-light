8e75222050909d9202e58dda7b68b8aa6deda985
Kirill <akulenko818@gmail.com>
2023-01-30 09:02:38 +0300

    circle view

--- Trafic light/ViewController.swift
+++ Trafic light/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  Trafic light
+//  Traffic light
 //
 //  Created by user on 30.01.2023.
 //
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redSection: UIView!
+    @IBOutlet weak var yellowSection: UIView!
+    @IBOutlet weak var greenSection: UIView!
+    
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       roundUpView()
     }
 
-
+    @IBAction func startButtonTapped() {
+    }
+    
+   
+    private func roundUpView() {
+        self.redSection.layer.cornerRadius = self.redSection.layer.bounds.height / 1.2
+        redSection.clipsToBounds = true
+        self.yellowSection.layer.cornerRadius = self.yellowSection.layer.bounds.height / 1.2
+        yellowSection.clipsToBounds = true
+        self.greenSection.layer.cornerRadius = self.greenSection.layer.bounds.height / 1.2
+        greenSection.clipsToBounds = true
+      
+    }
 }
 
