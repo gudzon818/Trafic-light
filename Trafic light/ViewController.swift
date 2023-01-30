4486b8d409599bb18fa2c831e364d568946e1622
Kirill <akulenko818@gmail.com>
2023-01-30 11:38:57 +0300

    finish

--- Trafic light/ViewController.swift
+++ Trafic light/ViewController.swift
@@ -7,23 +7,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum CurrentColor {
+    case red, yellow, green
+    
+}
 
+class ViewController: UIViewController {
+    
     @IBOutlet weak var redSection: UIView!
     @IBOutlet weak var yellowSection: UIView!
     @IBOutlet weak var greenSection: UIView!
     
     @IBOutlet weak var startButton: UIButton!
     
+    private var currentColor = CurrentColor.red
+    private let colorOn: Double = 1
+    private let colorOff = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       roundUpView()
-    }
-
-    @IBAction func startButtonTapped() {
+        startButton.layer.cornerRadius = 10
+        roundUpView()
+        
+        redSection.alpha = colorOff
+        yellowSection.alpha = colorOff
+        greenSection.alpha = colorOff
     }
     
-   
+    @IBAction func startButtonTapped() {
+        startButton.setTitle("Next", for: .normal)
+        
+        switch currentColor {
+        case .red :
+            redSection.alpha = colorOn
+            greenSection.alpha = colorOff
+            currentColor = .yellow
+        case .yellow:
+            yellowSection.alpha = colorOn
+            redSection.alpha = colorOff
+            currentColor = .green
+        case .green:
+            greenSection.alpha = colorOn
+            yellowSection.alpha = colorOff
+            currentColor = .red
+        }
+    }
+    
     private func roundUpView() {
         self.redSection.layer.cornerRadius = self.redSection.layer.bounds.height / 1.2
         redSection.clipsToBounds = true
@@ -31,7 +60,5 @@ class ViewController: UIViewController {
         yellowSection.clipsToBounds = true
         self.greenSection.layer.cornerRadius = self.greenSection.layer.bounds.height / 1.2
         greenSection.clipsToBounds = true
-      
     }
 }
-
